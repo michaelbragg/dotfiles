c80e27cf258adccec674806f677337ec1452b7f6
michaelbragg <accounts@michaelbragg.net>
2013-04-19 17:40:25 +0100

    Adds git alias' for git add and git commit -m

--- git/alias.zsh
+++ git/alias.zsh
@@ -1,2 +1,4 @@
-alias gc="git checkout"
+alias ga="git add"
+alias go="git checkout"
+alias gcm="git commit -m"
 alias glg="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
