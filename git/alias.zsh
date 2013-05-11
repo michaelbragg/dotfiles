d493fc6a6d339fc315bc983bec328dbb36f3a6f8
michaelbragg <accounts@michaelbragg.net>
2013-05-11 09:15:23 +0100

    Update git checkout alias

--- git/alias.zsh
+++ git/alias.zsh
@@ -1,4 +1,4 @@
 alias ga="git add"
-alias go="git checkout"
+alias gco="git checkout"
 alias gcm="git commit -m"
 alias glg="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
