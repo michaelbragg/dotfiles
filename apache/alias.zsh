8c85ac3859e6686e5e3ad966ae09c282948c6c4e
michaelbragg <accounts@michaelbragg.net>
2013-07-26 11:50:34 +0100

    Adds start and stop alias for apache

--- apache/alias.zsh
+++ apache/alias.zsh
@@ -1,2 +1,4 @@
+alias apache-start="sudo apachectl start"
+alias apache-stop="sudo apachectl stop"
 alias apache-restart="sudo apachectl restart"
 alias vhosts="open -e /etc/apache2/extra/httpd-vhosts.conf"
