4d845f6a605fe0bc78bc08b8a092b1d087a0bc32
michaelbragg <michael@michaelbragg.com>
2021-08-11 22:25:28 +0100

    Remove Apache alias
    Apache should be run via container

--- apache/alias.zsh
+++
@@ -1,4 +0,0 @@
-alias apache-start="sudo apachectl start"
-alias apache-stop="sudo apachectl stop"
-alias apache-restart="sudo apachectl restart"
-alias vhosts="open -e /etc/apache2/extra/httpd-vhosts.conf"
